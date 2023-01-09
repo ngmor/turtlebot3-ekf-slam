f5cb348ac9ad1e5049fd5d269fb64d110ddea121
Nick Morales <ngmorales97@gmail.com>
2023-01-09 14:26:51 -0600

    Vector2D frame_main usage

--- turtlelib/src/frame_main.cpp
+++ turtlelib/src/frame_main.cpp
@@ -6,6 +6,7 @@ using namespace turtlelib; //TODO - good practice?
 
 int main () {
 
+    //Transform2D
     Transform2D t_ab, t_bc;
 
     std::cout << "Enter transform T_{a,b}:\n";
@@ -18,5 +19,18 @@ int main () {
               << "T_{c,b}: " << t_bc.inv() << '\n'
               << "T_{a,c}: " << t_ab*t_bc << '\n'
               << "T_{c,a}: " << (t_ab*t_bc).inv() << '\n';
+    
+    //Vector2D
+    Vector2D v_b;
 
+    std::cout << "Enter vector v_b:\n";
+    std::cin >> v_b;
+    //TODO - normalize
+    std::cout << "v_bhat: " << '\n'
+              << "v_a: " << t_ab(v_b) << '\n'
+              << "v_b: " << v_b << '\n'
+              << "v_c: " << (t_bc.inv())(v_b) << '\n';
+
+    //Twist2D
+    //TODO
 }
