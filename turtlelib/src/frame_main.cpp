5c22f3115e0b6406fe7fd0a5be4d1526d049c994
Nick Morales <ngmorales97@gmail.com>
2023-01-09 10:24:40 -0600

    Added overloaded output operator for Vector2D

--- turtlelib/src/frame_main.cpp
+++ turtlelib/src/frame_main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "turtlelib/rigid2d.hpp"
 using std::cout;
+using std::cin;
 using namespace turtlelib; //TODO - good practice?
 
 int main () {
     cout << "Hello World!" << '\n';
 
     //Vector2D
-    Vector2D v_a {-1, 2};
+    Vector2D v_a;
+    int test;
 
+    cout << "Enter v_a and an int: " << '\n';
+    cin >> v_a >> test;
     cout << "v_a: " << v_a << '\n';
+    cout << "int: " << test << '\n';
 }
--- turtlelib/src/rigid2d.cpp
+++ turtlelib/src/rigid2d.cpp
@@ -10,4 +10,33 @@ namespace turtlelib
         os << '[' << v.x << ' ' << v.y << ']';
         return os;
     }
+
+    /// \brief input a 2 dimensional vector
+    ///   You should be able to read vectors entered as follows:
+    ///   [x y] or x y
+    /// \param is - stream from which to read
+    /// \param v [out] - output vector
+    std::istream & operator>>(std::istream & is, Vector2D & v) {
+        //remove leading whitespace
+        is >> std::ws;
+
+        char c = is.peek();
+
+        if (c == '[') {
+            //Remove leading bracket
+            c = is.get();
+        }
+
+        //Get actual data
+        is >> v.x >> v.y;
+
+        c = is.peek();
+
+        if (c == ']') {
+            //Remove trailing bracket
+            c = is.get();
+        }
+
+        return is;
+    };
 }
