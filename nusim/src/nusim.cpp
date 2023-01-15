da85f20d16549829f337399d7c20337ae3fbd03d
Nick Morales <ngmorales97@gmail.com>
2023-01-15 16:33:05 -0600

    Added obstacle parameters, added exception/code to throw it if obstacle list sizes differ.

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -2,6 +2,8 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
+#include <exception>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int64.hpp"
@@ -14,7 +16,6 @@
 using namespace std::chrono_literals;
 
 //Simple pose struct
-//TODO - replace with Transform2D?
 struct Pose2D {
     // x position
     double x = 0.0;
@@ -26,6 +27,21 @@ struct Pose2D {
     double theta = 0.0;
 };
 
+//Custom size mismatch exception
+class ObstacleListSizeMismatchException : public std::exception {
+private:
+    std::string msg_;
+public:
+    ObstacleListSizeMismatchException(int x, int y) :
+        msg_{
+            "Size mismatch between input obstacle coordinate lists (x: "
+            + std::to_string(x) + " elements, y: " + std::to_string(y) + " elements)"
+        }
+    {};
+    const char * what() const throw () { return msg_.c_str(); }
+};
+
+
 //Function prototypes
 geometry_msgs::msg::TransformStamped pose_to_transform(Pose2D pose);
 
@@ -34,26 +50,48 @@ geometry_msgs::msg::TransformStamped pose_to_transform(Pose2D pose);
 class NuSim : public rclcpp::Node {
 public:
     NuSim(): Node("nusim") {
+
         //Parameters
-        auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
-        param_desc.description = "The rate the simulation runs at.";
-        declare_parameter("rate", 200.0, param_desc);
+        auto param = rcl_interfaces::msg::ParameterDescriptor{};
+        param.description = "The rate the simulation runs at.";
+        declare_parameter("rate", 200.0, param);
         sim_rate_ = get_parameter("rate").get_parameter_value().get<double>();
         sim_interval_ = 1.0 / sim_rate_;
 
-        param_desc.description = "Initial x position of the robot.";
-        declare_parameter("x0", 0.0, param_desc);
+
+        param.description = "Initial x position of the robot.";
+        declare_parameter("x0", 0.0, param);
         pose_initial_.x = get_parameter("x0").get_parameter_value().get<double>();
 
-        param_desc.description = "Initial y position of the robot.";
-        declare_parameter("y0", 0.0, param_desc);
+        param.description = "Initial y position of the robot.";
+        declare_parameter("y0", 0.0, param);
         pose_initial_.y = get_parameter("y0").get_parameter_value().get<double>();
 
-        param_desc.description = "Initial rotation of the robot.";
-        declare_parameter("theta0", 0.0, param_desc);
+        param.description = "Initial rotation of the robot.";
+        declare_parameter("theta0", 0.0, param);
         pose_initial_.theta = get_parameter("theta0").get_parameter_value().get<double>();
         
         pose_current_ = pose_initial_;
+
+
+        param.description = "List of x positions of obstacles.";
+        declare_parameter("obstacles.x", std::vector<double> {}, param);
+        obstacles_x_ = get_parameter("obstacles.x").as_double_array();
+
+        param.description = "List of y positions of obstacles.";
+        declare_parameter("obstacles.y", std::vector<double> {}, param);
+        obstacles_y_ = get_parameter("obstacles.y").as_double_array();
+
+        //If vectors differ in size, exit node
+        if (obstacles_x_.size() != obstacles_y_.size()) {
+            throw ObstacleListSizeMismatchException(obstacles_x_.size(), obstacles_y_.size());
+        }
+
+        param.description = "Obstacle radius.";
+        declare_parameter("obstacles.r", 0.015, param);
+        obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
+
+
 
         //Timers
         timer_ = create_wall_timer(
@@ -79,14 +117,17 @@ public:
     }
 
 private:
-    double sim_rate_, sim_interval_;
-    Pose2D pose_initial_, pose_current_;
-    uint64_t timestep_ = 0;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr pub_timestep_;
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_reset_;
     rclcpp::Service<nusim::srv::Teleport>::SharedPtr srv_teleport_;
     std::unique_ptr<tf2_ros::TransformBroadcaster> broadcaster_;
+
+    double sim_rate_, sim_interval_;
+    uint64_t timestep_ = 0;
+    Pose2D pose_initial_, pose_current_;
+    std::vector<double> obstacles_x_, obstacles_y_;
+    double obstacles_r_;
 
     void timer_callback() {
         // //RCLCPP_INFO(get_logger(), "Publishing");
