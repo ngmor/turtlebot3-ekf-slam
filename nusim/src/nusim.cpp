099f22014ba8d40c39ce7f4d59ae1abb05874667
Nick Morales <ngmorales97@gmail.com>
2023-01-15 12:30:01 -0600

    Functional basic reset service for nusim node.

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -5,11 +5,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int64.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
 using namespace std::chrono_literals;
 
-//Basic node setup adapted from
-//https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html
 
 class NuSim : public rclcpp::Node {
 public:
@@ -21,31 +21,49 @@ public:
         sim_rate_ = get_parameter("rate").get_parameter_value().get<double>();
         sim_interval_ = 1.0 / sim_rate_;
 
+        //Timers
         timer_ = create_wall_timer(
             static_cast<std::chrono::microseconds>(static_cast<int>(sim_interval_*1000000.0)),
             std::bind(&NuSim::timer_callback, this)
         );
-        test_publisher_ = create_publisher<std_msgs::msg::String>("~/topic", 10);
-        timestep_publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+
+        //Publishers
+        pub_timestep_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+
+        // Services
+        srv_reset_ = create_service<std_srvs::srv::Empty>(
+            "~/reset",
+            std::bind(&NuSim::reset_callback, this, std::placeholders::_1, std::placeholders::_2)
+        );
     }
 private:
     double sim_rate_;
     double sim_interval_;
     uint64_t timestep_ = 0;
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr test_publisher_;
-    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_publisher_;
+    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr pub_timestep_;
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_reset_;
 
     void timer_callback() {
-        // auto message = std_msgs::msg::String();
-        // message.data = "Hello, world!";
         // //RCLCPP_INFO(get_logger(), "Publishing");
-        // test_publisher_->publish(message);
 
+        //Publish timestep and increment
         auto timestep_msg = std_msgs::msg::UInt64();
         timestep_msg.data = timestep_++;
-        timestep_publisher_->publish(timestep_msg);
+        pub_timestep_->publish(timestep_msg);
     }
+
+    void reset_callback(
+        const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+        std::shared_ptr<std_srvs::srv::Empty::Response> response
+    ) {
+        //Get rid of unused warnings
+        (void)request;
+        (void)response;
+
+        timestep_ = 0;
+    }
+
 };
 
 int main(int argc, char * argv[]) {
