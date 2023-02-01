8426a478262ef1c9f5079b635c4f51bc160bae74
Nick Morales <ngmorales97@gmail.com>
2023-01-31 23:46:18 -0600

    Added documentation for DiffDrive calculations

--- turtlelib/src/diff_drive.cpp
+++ turtlelib/src/diff_drive.cpp
@@ -8,10 +8,10 @@ namespace turtlelib
 
     void DiffDrive::calc_kinematic_coeff()
     {
-        coeff_ik_w_ = wheel_track_ / (2.0*wheel_radius_);
-        coeff_ik_x_ = 1.0 / (wheel_radius_);
-        coeff_fk_w_ = wheel_radius_ / wheel_track_;
-        coeff_fk_x_ = wheel_radius_ / 2.0;
+        coeff_ik_w_ = wheel_track_ / (2.0*wheel_radius_);   // doc/Kinematics.pdf Eqs 1.3 and 1.4
+        coeff_ik_x_ = 1.0 / (wheel_radius_);                // doc/Kinematics.pdf Eqs 1.3 and 1.4
+        coeff_fk_w_ = wheel_radius_ / wheel_track_;         // doc/Kinematics.pdf Eq 2.6
+        coeff_fk_x_ = wheel_radius_ / 2.0;                  // doc/Kinematics.pdf Eq 2.7
     }
 
     DiffDrive::DiffDrive(double wheel_track, double wheel_radius)
@@ -81,14 +81,14 @@ namespace turtlelib
 
         //Calculate produced body twist
         Twist2D body_twist {
-            coeff_fk_w_*(-wheel_delta.left + wheel_delta.right),    //w
-            coeff_fk_x_*(wheel_delta.left + wheel_delta.right),     //x
-            0.0                                                     //y
+            coeff_fk_w_*(-wheel_delta.left + wheel_delta.right),    //w, doc/Kinematics.pdf Eq 2.6
+            coeff_fk_x_*(wheel_delta.left + wheel_delta.right),     //x, doc/Kinematics.pdf Eq 2.7
+            0.0                                                     //y, doc/Kinematics.pdf Eq 2.5
         };
 
         //integrate twist and compose it with the current location transformation
         //Twbp = Twb * Tbbp
-        config_.location *= integrate_twist(body_twist);
+        config_.location *= integrate_twist(body_twist);    //doc/Kinematics.pdf Eq 3.1-3.7
 
         //Update wheel positions to new provided positions
         config_.wheel_pos = new_wheel_pos;
@@ -99,6 +99,7 @@ namespace turtlelib
     Wheel DiffDrive::get_required_wheel_vel(const Twist2D & twist) const
     {
         //twists with any y velocity cannot be achieved without wheels sliding
+        //(See discussion on nullspace of R matrix, doc/Kinematics.pdf)
         //throw an exception if this is requested
         if (twist.y != 0.0) {
             throw std::logic_error(
@@ -107,8 +108,8 @@ namespace turtlelib
         }
 
         return Wheel {
-            -coeff_ik_w_*twist.w + coeff_ik_x_*twist.x,     //left
-            coeff_ik_w_*twist.w + coeff_ik_x_*twist.x       //right
+            -coeff_ik_w_*twist.w + coeff_ik_x_*twist.x,     //left, doc/Kinematics.pdf Eq 1.3
+            coeff_ik_w_*twist.w + coeff_ik_x_*twist.x       //right, doc/Kinematics.pdf Eq 1.4
         };
     }
 
