b3edb2895302f146d084a647a6d2caa1be2c34b3
Nick Morales <ngmorales97@gmail.com>
2023-02-01 23:11:33 -0600

    Added parameters for turtle_control node and exception if they are not provided.

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -13,6 +13,37 @@ public:
   TurtleControl()
   : Node ("turtle_control")
   {
+    //Parameters
+    auto param = rcl_interfaces::msg::ParameterDescriptor{};
+    
+    param.description = "The wheel track in meters (REQUIRED)";
+    declare_parameter("wheel_track", 0.0, param);
+    double wheel_track = get_parameter("wheel_track").get_parameter_value().get<double>();
+
+    param.description = "The wheel radius in meters (REQUIRED)";
+    declare_parameter("wheel_radius", 0.0, param);
+    double wheel_radius = get_parameter("wheel_radius").get_parameter_value().get<double>();
+
+    bool required_parameters_received = true;
+
+    if (wheel_track <= 0) {
+      RCLCPP_ERROR_STREAM(get_logger(), "Invalid wheel track provided: " << wheel_track);
+      required_parameters_received = false;
+    }
+
+    if (wheel_radius <= 0) {
+      RCLCPP_ERROR_STREAM(get_logger(), "Invalid wheel radius provided: " << wheel_radius);
+      required_parameters_received = false;
+    }
+
+    if (!required_parameters_received) {
+      throw std::logic_error(
+        "Required parameters were not received or were invalid. Please provide valid parameters."
+      );
+    }
+
+    //Initialize turtlebot with input parameters
+    turtlebot_ = DiffDrive {wheel_track, wheel_radius};
 
     RCLCPP_INFO_STREAM(get_logger(), "turtle_control node started");
   }
