064d509f014ebf543ac1829529ebee934961b35b
Nick Morales <ngmorales97@gmail.com>
2023-02-03 13:31:51 -0600

    Added control service for circle node

--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nuturtle_control/srv/circle.hpp"
 
 /// \brief Moves the turtlebot in a circle with a configurable velocity/radius
 class Circle : public rclcpp::Node
@@ -15,15 +16,61 @@ public:
 
     //Parameters
     auto param = rcl_interfaces::msg::ParameterDescriptor{};
+
+    param.description = "The frequency of publishing cmd_vel messages (Hz).";
+    declare_parameter("frequency", 100.0, param);
+    sim_freq_ = get_parameter("frequency").get_parameter_value().get<double>();
+    sim_interval_ = 1.0 / sim_freq_;
     
+    //Timers
+    timer_ = create_wall_timer(
+      static_cast<std::chrono::microseconds>(static_cast<int>(sim_interval_ * 1000000.0)),
+      std::bind(&Circle::timer_callback, this)
+    );
+
+    // Services
+    srv_control_ = create_service<nuturtle_control::srv::Circle>(
+      "control",
+      std::bind(&Circle::control_callback, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
 
     RCLCPP_INFO_STREAM(get_logger(), "circle node started");
   }
 private:
-  
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Service<nuturtle_control::srv::Circle>::SharedPtr srv_control_;
+
+  double sim_freq_, sim_interval_;
+  double velocity_, radius_;
+  bool publish_ = false;
+
+  /// \brief main timer loop for publishing cmd_vel messages
+  void timer_callback()
+  {
+    if (publish_)
+    {
+
+    }
+  }
+
+  void control_callback(
+    const std::shared_ptr<nuturtle_control::srv::Circle::Request> request,
+    std::shared_ptr<nuturtle_control::srv::Circle::Response> response
+  )
+  {
+    //Get rid of unused warnings
+    (void)response;
+
+    velocity_ = request->velocity;
+    radius_ = request->radius;
+
+    //start publishing
+    publish_ = true;
+  }
 };
 
-/// \brief Run the node 
+/// \brief Run the node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
