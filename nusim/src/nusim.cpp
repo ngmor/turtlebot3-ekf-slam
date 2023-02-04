40701c44c25530d01d7dba7b70ff812e9a82c357
Nick Morales <ngmorales97@gmail.com>
2023-02-03 20:21:03 -0600

    Refactored nusim node to use turtlelib

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -39,8 +39,15 @@
 #include "nusim/srv/teleport.hpp"
 #include "visualization_msgs/msg/marker.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
+#include "nuturtlebot_msgs/msg/wheel_commands.hpp"
+#include "nuturtlebot_msgs/msg/sensor_data.hpp"
+#include "turtlelib/diff_drive.hpp"
 
 using namespace std::chrono_literals;
+using turtlelib::Vector2D;
+using turtlelib::Transform2D;
+using turtlelib::DiffDrive;
+using turtlelib::DiffDriveConfig;
 
 // Constants
 //TODO - define somewhere centrally?
@@ -48,43 +55,9 @@ constexpr std::string_view WORLD_FRAME = "nusim/world";
 constexpr std::string_view ROBOT_GROUND_TRUTH_FRAME = "red/base_footprint";
 constexpr double OBSTACLE_HEIGHT = 0.25;
 
-/// \brief simple 2D pose struct
-struct Pose2D
-{
-  // x position
-  double x = 0.0;
-
-  // y position
-  double y = 0.0;
-
-  // angle
-  double theta = 0.0;
-};
-
-/// \brief custom obstacle list size mismatch exception
-class ObstacleListSizeMismatchException : public std::exception
-{
-private:
-  std::string msg_;
-
-public:
-  /// \brief construct a custom obstacle list size mismatch exception message
-  /// \param x - size of x list
-  /// \param y - size of y list
-  ObstacleListSizeMismatchException(int x, int y)
-  : msg_{
-      "Size mismatch between input obstacle coordinate lists (x: " +
-      std::to_string(x) + " elements, y: " + std::to_string(y) + " elements)"
-  } {}
-
-  /// \brief return exception message
-  /// \return - exception message pointer
-  const char * what() const throw () {return msg_.c_str();}
-};
-
 
 //Function prototypes
-geometry_msgs::msg::TransformStamped pose_to_transform(Pose2D pose);
+geometry_msgs::msg::TransformStamped pose_to_transform(Transform2D pose);
 
 
 /// \brief Runs the simulation for the NUTurtle.
@@ -103,21 +76,40 @@ public:
     sim_rate_ = get_parameter("rate").get_parameter_value().get<double>();
     sim_interval_ = 1.0 / sim_rate_;
 
+    //Check if required parameters were provided
+    bool required_parameters_received = true;
 
+    param.description = "The wheel track width in meters (REQUIRED)";
+    declare_parameter("track_width", 0.0, param);
+    double wheel_track = get_parameter("track_width").get_parameter_value().get<double>();
+
+    if (wheel_track <= 0) {
+      RCLCPP_ERROR_STREAM(get_logger(), "Invalid wheel track provided: " << wheel_track);
+      required_parameters_received = false;
+    }
+
+    param.description = "The wheel radius in meters (REQUIRED)";
+    declare_parameter("wheel_radius", 0.0, param);
+    double wheel_radius = get_parameter("wheel_radius").get_parameter_value().get<double>();
+
+    if (wheel_radius <= 0) {
+      RCLCPP_ERROR_STREAM(get_logger(), "Invalid wheel radius provided: " << wheel_radius);
+      required_parameters_received = false;
+    }
+
+    Vector2D translation_initial;
+    
     param.description = "Initial x position of the robot (m).";
     declare_parameter("x0", 0.0, param);
-    pose_initial_.x = get_parameter("x0").get_parameter_value().get<double>();
+    translation_initial.x = get_parameter("x0").get_parameter_value().get<double>();
 
     param.description = "Initial y position of the robot (m).";
     declare_parameter("y0", 0.0, param);
-    pose_initial_.y = get_parameter("y0").get_parameter_value().get<double>();
+    translation_initial.y = get_parameter("y0").get_parameter_value().get<double>();
 
     param.description = "Initial rotation of the robot (rad).";
     declare_parameter("theta0", 0.0, param);
-    pose_initial_.theta = get_parameter("theta0").get_parameter_value().get<double>();
-
-    pose_current_ = pose_initial_;
-
+    double rotation_initial = get_parameter("theta0").get_parameter_value().get<double>();
 
     param.description =
       "List of x starting positions of obstacles (m). Arbitrary length, but must match length of y.";
@@ -131,7 +123,11 @@ public:
 
     //If vectors differ in size, exit node
     if (obstacles_x_.size() != obstacles_y_.size()) {
-      throw ObstacleListSizeMismatchException(obstacles_x_.size(), obstacles_y_.size());
+        RCLCPP_ERROR_STREAM(get_logger(),
+          "Size mismatch between input obstacle coordinate lists (x: " << obstacles_x_.size() <<
+          " elements, y: " << obstacles_y_.size() << " elements)"
+        );
+        required_parameters_received = false;
     }
 
     param.description =
@@ -139,6 +135,13 @@ public:
     declare_parameter("obstacles.r", 0.015, param);
     obstacles_r_ = get_parameter("obstacles.r").get_parameter_value().get<double>();
 
+
+    //Abort if any required parameters were not provided
+    if (!required_parameters_received) {
+      throw std::logic_error(
+        "Required parameters were not received or were invalid. Please provide valid parameters."
+      );
+    }
 
     //Timers
     timer_ = create_wall_timer(
@@ -163,6 +166,16 @@ public:
     //Broadcasters
     broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 
+    //Other variables
+    turtlebot_ = DiffDrive {
+      wheel_track,
+      wheel_radius,
+      Transform2D{
+        translation_initial,
+        rotation_initial
+      }
+    };
+
     init_obstacles();
 
     RCLCPP_INFO_STREAM(get_logger(), "nusim node started");
@@ -172,13 +185,14 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr pub_timestep_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr pub_obstacles_;
+
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_reset_;
   rclcpp::Service<nusim::srv::Teleport>::SharedPtr srv_teleport_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> broadcaster_;
 
   double sim_rate_, sim_interval_;
   uint64_t timestep_ = 0;
-  Pose2D pose_initial_, pose_current_;
+  DiffDrive turtlebot_ {0.16, 0.033}; //Default values, to be overwritten in constructor
   std::vector<double> obstacles_x_, obstacles_y_;
   double obstacles_r_;
   visualization_msgs::msg::MarkerArray obstacle_markers_;
@@ -192,7 +206,7 @@ private:
     pub_timestep_->publish(timestep_msg);
 
     //Broadcast current transform of robot
-    auto tf = pose_to_transform(pose_current_);
+    auto tf = pose_to_transform(turtlebot_.config().location);
     tf.header.stamp = get_clock()->now();
     broadcaster_->sendTransform(tf);
 
@@ -216,7 +230,7 @@ private:
     timestep_ = 0;
 
     //Reset current pose
-    pose_current_ = pose_initial_;
+    turtlebot_.reset();
   }
 
   /// \brief teleport the actual robot to a specified pose. callback for ~/teleport service.
@@ -231,9 +245,16 @@ private:
     (void)response;
 
     //Teleport current pose
-    pose_current_.x = request->x;
-    pose_current_.y = request->y;
-    pose_current_.theta = request->theta;
+    turtlebot_.set_config(DiffDriveConfig{
+      Transform2D{
+        Vector2D{
+          request->x,
+          request->y
+        },
+        request->theta
+      },
+      turtlebot_.config().wheel_pos
+    });
   }
 
   /// \brief initialize the obstacle marker array.
@@ -286,18 +307,18 @@ private:
 /// \brief format a pose as a TransformStamped message
 /// \param pose - pose to turn into a transform
 /// \return - TransformStamped message for the pose, with no timestamp
-geometry_msgs::msg::TransformStamped pose_to_transform(Pose2D pose)
+geometry_msgs::msg::TransformStamped pose_to_transform(Transform2D pose)
 {
   geometry_msgs::msg::TransformStamped tf;
 
   tf.header.frame_id = WORLD_FRAME;
   tf.child_frame_id = ROBOT_GROUND_TRUTH_FRAME;
 
-  tf.transform.translation.x = pose.x;
-  tf.transform.translation.y = pose.y;
+  tf.transform.translation.x = pose.translation().x;
+  tf.transform.translation.y = pose.translation().y;
 
   tf2::Quaternion q;
-  q.setRPY(0, 0, pose.theta);
+  q.setRPY(0, 0, pose.rotation());
   tf.transform.rotation.x = q.x();
   tf.transform.rotation.y = q.y();
   tf.transform.rotation.z = q.z();
