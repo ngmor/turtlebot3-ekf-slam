ee613ecd23d04dde4a694a1f76120a638f557e2f
Nick Morales <ngmorales97@gmail.com>
2023-03-08 14:09:26 -0600

    Circle fitting up until singular value decomposition

--- turtlelib/src/circle_detection.cpp
+++ turtlelib/src/circle_detection.cpp
@@ -16,7 +16,7 @@ namespace turtlelib
 
         const auto num_points = points.size();
 
-        // Collect points into x and y vectors
+        //Collect points into x and y vectors
         std::vector<double> x_coords;
         std::vector<double> y_coords;
         x_coords.reserve(num_points);
@@ -30,25 +30,34 @@ namespace turtlelib
         //Calculate centroid of points
         const Vector2D centroid {mean(x_coords), mean(y_coords)};
 
-        //Shift coordinates so that the centroid is at the origin
-        for (auto & x : x_coords) {
-            x -= centroid.x;
-        }
-        for (auto & y : y_coords) {
-            y -= centroid.y;
-        }
-
-        //Compute z values
         std::vector<double> z_vals (num_points, 0.0);
+        arma::mat Z {num_points, 4, arma::fill::zeros};
 
         for (size_t i = 0; i < num_points; i++) {
-            z_vals[i] = x_coords[i]*x_coords[i] + y_coords[i]*y_coords[i];
+            //Init references
+            auto & x = x_coords[i];
+            auto & y = y_coords[i];
+            auto & z = z_vals[i];
+
+            //Shift coordinates so that the centroid is at the origin
+            x -= centroid.x;
+            y -= centroid.y;
+
+            //Compute z values
+            z = x*x + y*y;
+
+            //Form the data matrix Z from the data points
+            Z(i, 0) = z;
+            Z(i, 1) = x;
+            Z(i, 2) = y;
+            Z(i, 3) = 1.0;
         }
 
         //Compute the mean of z
         const auto z_mean = mean(z_vals);
 
-
+        //Form the moment matrix M
+        arma::mat M = (1.0 / num_points) * Z.t() * Z;
 
         return {};
     }
